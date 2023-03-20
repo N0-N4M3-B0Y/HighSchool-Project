1475750fa5b59ebde75d2ac3dd550be7aa0380a9
Pedro Artur (Motsu) <71746213+N0-N4M3-B0Y@users.noreply.github.com>
2023-03-20 09:33:51 -0300

    Js Done

--- HighSchool/sript.js
+++ HighSchool/sript.js
@@ -1,1 +1,21 @@
-//WIP
+let loginBtn = document.getElementById('login-btn')
+let email = document.getElementById('e-mail')
+let password = document.getElementById('password')
+let passwordRegex = /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
+let mailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+
+loginBtn.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    console.log(mailRegex.test(email.value))
+    console.log(passwordRegex.test(password.value))
+
+    if(!mailRegex.test(email.value) || !passwordRegex.test(password.value)) {
+        alert('Email ou senha Invalido')
+    }
+})
+
+/*
+Programador, que estiver lendo isso, lembresse do que nossa professora disse, NÃO USE SÓ O REQUIRED MIZERA, faça a validação no JS também <3
+-Motsu
+*/
